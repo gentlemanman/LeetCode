32127fc4eaf8e48f217f04fd25e329936f8a8aae
zhaohan <540583339@qq.com>
2018-02-26 12:18:20 +0800

    Add files via upload

---
+++ 51_N-Queens.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+class Solution{
+public:
+    vector<vector<string> > solveNQueens(int n){
+        vector< vector<string> > res;
+        vector<int> pos(n, -1);
+        solveNQueensDFS(pos, 0, res);
+        return res;
+    }
+    void solveNQueensDFS(vector<int> &pos, int row, vector<vector<string> > &res){
+        int n = pos.size();
+        if (row == n){
+            vector<string> out(n, string(n,'.'));
+            for (int i=0; i<n; ++i){
+                out[i][pos[i]] = 'Q';
+            }
+            res.push_back(out);
+        }
+        else{
+            for (int col=0; col<n; ++col){
+                if (isValid(pos, row, col)){
+                    pos[row] = col;
+                    solveNQueensDFS(pos, row+1, res);
+                    pos[row] = -1;
+                } 
+            }
+        }
+    }
+    bool isValid(vector<int> &pos, int row, int col){
+        for (int i=0; i<row; ++i){
+           //  if(col==pos[i] || abs(row-i) == abs(col-pos[i])){
+            if (col==pos[i]){  
+            return false;
+            }
+        }
+        return true;
+    }
+};
+int main(){
+    Solution s;
+    int n = 4;
+    vector<vector<string> > a = s.solveNQueens(n);
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+        {
+            cout<<a[i][j]<<endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
